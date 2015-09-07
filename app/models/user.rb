aaf296991230cfc752678e795318e249f84a5f92
Tracy Kaneshiro <tkaneshiro@gmail.com>
2015-09-07 12:18:19 -0500

    bug fix for pins belonging to deleted users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :pins
+  has_many :pins, dependent: :destroy
   validates :name, presence:true
 end
