f3366cce86e1f9a236b556825f4b9f21ec3adef1
Tracy Kaneshiro <tkaneshiro@gmail.com>
2015-09-07 11:10:33 -0500

    adding validations and name field

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -22,7 +22,7 @@ class PinsController < ApplicationController
     if @pin.save
       redirect_to @pin, notice: 'Pin was successfully created.'
     else
-      format.html render :new 
+      render action: 'new'
     end
   end
 
@@ -30,7 +30,7 @@ class PinsController < ApplicationController
     if @pin.update(pin_params)
       redirect_to @pin, notice: 'Pin was successfully updated.' 
     else
-      format.html render :edit 
+      render action: 'edit' 
     end
   end
 
--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,7 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :image, styles: { :medium => "300x300", :thumb => "100x100"}
+	validates :image, presence:true
+	validates :description, presence:true
 	validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 end
